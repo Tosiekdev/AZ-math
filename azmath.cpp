edc06f748669ff1ded2e1d17528c1c709f729b48
Tosiek <antonizieciak@gmail.com>
2023-03-22 20:24:07 +0100

    enter

--- azmath.cpp
+++ azmath.cpp
@@ -1,3 +1,4 @@
+
 #include "azmath.h"
 
 #include <utility>
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/tosiek/Documents/II_semestr/PP2/az_math
+# Install script for directory: /home/tosiek/AZ-math
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tosiek/Documents/II_semestr/PP2/az_math/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tosiek/AZ-math/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
